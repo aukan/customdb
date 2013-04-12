a921c9cc752c69866d4489d94db6c02ac8c088cc
Pablo Gonzalez <pbglezc@gmail.com>
2013-04-12 17:30:13 +0000

    Refactor a little bit, to debug easier.

---
+++ iterator.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+
+var iterateOnFile = function iterateOnFile (onIteration, onEnd) {
+    // Read file
+    var dbFile = fs.createReadStream(process.argv[2], { bufferSize: 64 * 1024 });
+    var recordBuffer = "";
+
+    // Iterate on file and execute functions
+    dbFile.on('data', function(data, algo) {
+        var records, record;
+        var i, j;
+        var stringData = data.toString();
+        var limit;
+
+        records = stringData.split('\n');
+
+        if (records.length === 1) {
+            recordBuffer += stringData;
+            return;
+        } else if (recordBuffer.length > 0) {
+            records[0] = recordBuffer + records[0];
+            recordBuffer = "";
+        }
+
+        limit = records.length - 1;
+
+        for( i=0; i < limit; i+=1 ){
+            if (records[i].match(/[^\s]/)) {
+                record = JSON.parse(records[i]);
+                onIteration.call(this, record);
+            }
+        }
+
+        recordBuffer += records[limit];
+    });
+
+    dbFile.on('end', onEnd || function () {} );
+}
+
+exports.iterateOnFile = iterateOnFile;
